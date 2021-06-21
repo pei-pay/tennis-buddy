3db15df0cb172cc7aee0ebcfbbe03e336c01d302
pei-pay <gleektennis@icloud.com>
2021-06-21 17:57:13 +0900

    firebase deploy error修正

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 // github pages の為にdocsフォルダへデプロイ先を変更。
 module.exports = {
   outputDir: 'docs',
-  publicPath: process.env.NODE_ENV === 'production' ? '/tennis-buddy/' : './',
+  publicPath: process.env.NODE_ENV === 'production' ? '/tennis-buddy/' : '/',
 }
